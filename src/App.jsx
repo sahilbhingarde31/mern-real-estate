7e7a85cbe6c0ee8d33e50aa04d83f9ec19dad6be
Sahil Bhingarde <sahilbhingarde.sb31@gmail.com>
2024-10-30 22:31:43 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-red-500">APP</h1>
+      <h1 align="center" className="text-red-500">HELL THIS IS MY FIRST WEBSITE</h1>
+      <div className="header">
+        <ul>
+          <li><a href=''>home</a></li>
+          <li><a href='#'>about us</a></li>
+          <li><a href='#'>Login</a></li>
+        </ul>
+      </div>
+    
+    </div>
+  )
+}
