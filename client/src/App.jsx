cf7609a1dca78ac1d857992aa48576b6343733b7
Sahil Bhingarde <sahilbhingarde.sb31@gmail.com>
2024-12-01 23:49:04 +0530

    complete sign up page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,8 +12,8 @@ export default function App() {
      <Header/>
      <Routes>
      <Route path='/' element={<Home/>}></Route>
-     <Route path='/signin' element={<Signin/>}></Route>
-     <Route path='/signup' element={<Signup/>}></Route>
+     <Route path='/sign-in' element={<Signin/>}></Route>
+     <Route path='/sign-up' element={<Signup/>}></Route>
      <Route path='/about' element={<About/>}></Route>
      <Route path='/profile' element={<Profile/>}></Route>
 
