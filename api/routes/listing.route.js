f02025c8ab26b9b4a156cd9d2d7a1c4e771790b3
Sahil Bhingarde <sahilbhingarde.sb31@gmail.com>
2025-03-29 23:02:53 +0530

    complete user delete user listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,10 @@
 import expresss from 'express';
-import { createListing } from '../controllers/listing.controller.js';
+import { createListing, deletelisting } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = expresss.Router();
 
 router.post('/create', verifyToken, createListing);
+router.delete('/delete/:id', verifyToken , deletelisting);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -131,6 +131,22 @@ export default function Profile() {
     } catch (error) {
       setShowListingError(true);
     }
+  };
+
+  const handleListingDelete = async(listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method:'DELETE',
+      });
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message);
+    }
   }
   return (
     <div className="p-3 max-w-lg mx-auto my-9 p-3 max-w-lg mx-auto shadow-lg shadow:opacity-0 bg-slate-200">
@@ -218,7 +234,7 @@ export default function Profile() {
             <p>{listing.name}</p>
             </Link>
             <div className="flex flex-col items-center">
-              <button className="text-red-700 font-semibold hover:underline hover:opacity-75">DELETE</button>
+              <button type="button" onClick={() => handleListingDelete(listing._id)} className="text-red-700 font-semibold hover:underline hover:opacity-75">DELETE</button>
               <button className="text-green-700 font-semibold hover:underline hover:opacity-75">EDIT</button>
             </div>
          </div>);})}
