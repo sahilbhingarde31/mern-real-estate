5e8721cdf7f25dd7fcf0b1386b8325f752b6c220
Sahil Bhingarde <sahilbhingarde.sb31@gmail.com>
2025-03-30 20:14:57 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import expresss from 'express';
-import { createListing, deletelisting } from '../controllers/listing.controller.js';
+import { createListing, deletelisting, updateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = expresss.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken , deletelisting);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
